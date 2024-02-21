60a81c4eb8a12768afe963a8d1d6c05721898bf7
Cameron Angelilli <115901280+Camerxns@users.noreply.github.com>
2024-02-21 15:38:47 -0500

    Working with breeze and updating dashboard

--- Hotel_Won-By-Ton/routes/web.php
+++ Hotel_Won-By-Ton/routes/web.php
@@ -29,6 +29,37 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/booking', function () {
+    return view('booking');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/room', function () {
+    return view('room');
+});
+Route::get('/service', function () {
+    return view('service');
+});
+Route::get('/team', function () {
+    return view('team');
+});
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/payment', function(){
+    return view('payment');
+});
 
 require __DIR__.'/auth.php';
 Route::get('/', function () {
