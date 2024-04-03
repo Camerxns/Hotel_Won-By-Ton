32eaacb4cb3f9d0778ca61d7283b5c2f48decfd3
Cameron Angelilli <115901280+Camerxns@users.noreply.github.com>
2024-04-03 15:11:43 -0400

    Added reservation cancellation for users

--- Hotel_Won-By-Ton/resources/views/showReservations.blade.php
+++ Hotel_Won-By-Ton/resources/views/showReservations.blade.php
@@ -20,7 +20,10 @@
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Check-in Date</th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Check-out Date</th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price Paid</th>
+<<<<<<< Updated upstream
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+=======
+>>>>>>> Stashed changes
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
@@ -30,7 +33,20 @@
                                         <td class="px-6 py-4 whitespace-nowrap">{{ $reservation->RoomID }}</td>
                                         <td class="px-6 py-4 whitespace-nowrap">{{ $reservation->CheckinDate }}</td>
                                         <td class="px-6 py-4 whitespace-nowrap">{{ $reservation->CheckoutDate }}</td>
+<<<<<<< Updated upstream
                                         <td class="px-6 py-4 whitespace-nowrap">${{ $reservation->Price }}</td>
+=======
+                                        <td class="px-6 py-4 whitespace-nowrap">{{ $reservation->Price }}</td>
+                                        <td><form action="{{ route('reservations.destroy', $reservation->ReservationID) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="17.64" height="17.64" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                                </svg>
+                                            </button>
+                                        </form></td>
+>>>>>>> Stashed changes
                                     </tr>
                                 @endforeach
                             </tbody>
