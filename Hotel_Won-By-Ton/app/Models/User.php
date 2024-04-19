41d000939d6499637b879e1773009f7bffe170f6
JakobGWhorl <jakesterw1@hotmail.com>
2024-04-19 13:12:35 -0400

    points and rewards system

--- Hotel_Won-By-Ton/app/Models/User.php
+++ Hotel_Won-By-Ton/app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable
         'remember_token',
         'created_at',
         'updated_at',
-        'AccessLevel'
+        'AccessLevel',
+        'points'
     ];
 
 
