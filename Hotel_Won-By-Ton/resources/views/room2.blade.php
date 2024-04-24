ed20c14458260009c77ed9f0101020906b8850d1
JakobGWhorl <jakesterw1@hotmail.com>
2024-04-24 14:00:10 -0400

    fixed link and styling for rewards page

--- Hotel_Won-By-Ton/resources/views/room2.blade.php
+++ Hotel_Won-By-Ton/resources/views/room2.blade.php
@@ -9,6 +9,7 @@
     <title>Rooms</title>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
 <style>
 <style>
@@ -104,6 +105,7 @@
                     <div class="desc-item"> {{ $room->RoomType }}</div>
                     <div class="desc-item"> {{ $room->Description }}</div>
                     <div class="desc-item"> {{ $room->Availability }}</div>
+                    <div class="desc-item"> <i class='fas fa-male' style='font-size:24px'></i> : {{ $room->Capacity }} </div>
                     <div class="desc-item">Price Per Night: ${{ $room->Price }}</div>
                     <div class="button-container">
                         <a class="btn btn-sm btn-dark rounded" href="http://127.0.0.1:8000/booking">Book Now</a>
